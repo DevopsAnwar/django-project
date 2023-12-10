7b80870fc98927e09c530b5cad084e13c3b69499
anwar <anwartfaisal86@gmail.com>
2023-12-09 21:29:50 -0600

    updating settings.py

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -85,10 +85,10 @@ WSGI_APPLICATION = 'cblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'database1', # database name in RDS is written here
+        'NAME': 'database-1', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
-        'PASSWORD': 'Tech1234',
-        'HOST': 'database1.cewvxnnnahzx.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'PASSWORD': 'Admin12345',
+        'HOST': 'database-1-anwar.cllkljv1hjzt.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
@@ -144,7 +144,7 @@ LOGIN_REDIRECT_URL = "blog:list"
 LOGIN_URL = "login"
 
 
-AWS_STORAGE_BUCKET_NAME = 'blog-s3-techtorial' # please enter your s3 bucket name
+AWS_STORAGE_BUCKET_NAME = 'project-bucket-anwar' # please enter your s3 bucket name
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_REGION_NAME = "us-east-1" # please enter your s3 region 
 AWS_DEFAULT_ACL = 'public-read'
